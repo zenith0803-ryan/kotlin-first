551182608ad3398f3941bd2d2b9a8646c6f14feb
Ryan <ryan.shin@kakaocorp.com>
2020-05-19 22:12:41 +0900

    심플 자바빈

---
+++ src/com/kotlin/two/two/Person.java
@@ -0,0 +1,14 @@
+package com.kotlin.two.two;
+
+//간단한 JavaBean 클래스
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
