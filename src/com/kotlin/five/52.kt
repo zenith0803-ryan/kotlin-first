caad8ae8cbfe7c8f4f9576cd1a9b1d9c0e0cd5dd
Ryan <ryan.shin@kakaocorp.com>
2020-06-08 00:30:48 +0900

    5.2.2, 5.2.3

--- src/com/kotlin/five/52.kt
+++ src/com/kotlin/five/52.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>){
     println( list.filter { it % 2 == 0 })
     println(list.map { it * it })
 
-    val people = listOf(Person("Alice", 29), Person("Bob", 31), Person("Ryan", 42))
+    val people = listOf(Person("Alice", 27), Person("Bob", 31), Person("Ryan", 42), Person("Carol", 31))
     println(people.map {it.name})
     println(people.map(Person::name))
     println(people.filter{ it.age>=30}.map(Person::name))
@@ -23,4 +23,17 @@ fun main(args: Array<String>){
 
     val numbers = mapOf( 0 to "zero" , 1 to "one")
     println( numbers.mapValues { it.value.toUpperCase() })
+
+    val canBeInClub27 = {p : Person  -> p.age<=27 }
+    println( people.all(canBeInClub27))
+    println( people.any(canBeInClub27))
+    println(people.count(canBeInClub27))
+    println(people.filter(canBeInClub27).size) //지양하자 -> 중간 컬렉션이 생겨서 비효율적
+    println(people.find(canBeInClub27))
+
+    println(people.groupBy { it.age })
+
+    val list2 = listOf("a", "ab", "b")
+    println(list2.groupBy(String::first))
+
 }
